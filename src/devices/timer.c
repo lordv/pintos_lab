aad305b7508b9d2d00d5c354fad7a11adf6b037c
nihar <niharsettipalli@gmail.com>
2010-08-31 16:33:09 +0530

    changed timer.c

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -89,12 +89,8 @@ timer_elapsed (int64_t then)
 	void
 timer_sleep (int64_t ticks) 
 {
-	int64_t start = timer_ticks ();
-
 	ASSERT (intr_get_level () == INTR_ON);
 	thread_wait(ticks);
-	//while (timer_elapsed (start) < ticks) 
-	//  thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
